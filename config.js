b16549904cd6d60a95d6b8fb453bfa812cd400ba
Akshay Singh <akshaysingh.1713@gmail.com>
2016-11-25 00:42:13 +0530

    remote db config vars

--- config.js
+++ config.js
@@ -1,10 +1,16 @@
+const db_username = process.env.DB_USER_NAME || 'root';
+const db_password = process.env.DB_PASSWORD || 'ksh';
+const db_name = process.env.DB_NAME || 'kex_test';
+const db_host = process.env.DB_HOST || '127.0.0.1';
+const db_port = process.env.DB_PORT || 3307;
+
 const config = {
 	db: {
-		username: 'root',
-		password: 'root',
-		name: 'knex_test',
-		host: '127.0.0.1',
-		port: 3300
+		username: db_username,
+		password: db_password,
+		name: db_name,
+		host: db_host,
+		port: db_port
 	},
 	oauth: {
 		clientID: '666643282620-93og63j8ljr7b2ufb2gcqkqdkp71f3q6.apps.googleusercontent.com',
